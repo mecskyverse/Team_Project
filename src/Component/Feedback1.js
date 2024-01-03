d74afef28c13450ed4640b4cc40eb5749fd0aec4
NiharikaChoudhary <88475866+NiharikaChoudhary@users.noreply.github.com>
2024-01-03 22:28:27 +0530

    Add files via upload

---
+++ src/Component/Feedback1.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Feedback1 = () => {
+  return (
+   <>
+   
+          <div className="card-body">
+          <div>
+                <h6 class="font-weight-bold  mb-1 d-inline">Rohan sharma </h6> <span className='text-muted ml-3'>26 Apr, 2023</span>
+                <p class="text-muted mt-1 small mb-0">
+                xyz976@gmail.com  /  Mobile Number
+                </p>
+            </div>
+            <div className='mt-2'>
+            <p className='font-weight-bold small d-inline'>
+                Subject:
+            </p>
+            <p className='d-inline small'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            </p>
+            </div>
+            
+            <p className="mt-3 mb-4 pb-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip consequat.
+            </p>
+          <hr />
+          </div>
+    
+   </>
+  )
+}
+
+export default Feedback1
---
+++ src/Component/Responses.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Feedback1 from './Feedback1';
+const Responses = () => {
+  return (
+   <>
+   <Navbar/>
+   <section >
+
+  <div className="container my-3 py-5">
+    <h2>Responses</h2>
+    <div className="row d-flex mt-4 justify-content-center">
+        
+      <div className="col-md-12 col-lg-10 col-xl-8">
+      <h3>Commends</h3>
+       <Feedback1/>
+       <Feedback1/>
+       <Feedback1/>
+       <Feedback1/>
+       <Feedback1/>
+       <Feedback1/>
+      </div>
+    </div>
+  </div>
+</section>
+
+   <Footer/>
+   </>
+  )
+}
+
+export default Responses
