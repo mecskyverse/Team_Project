778f4ec0d4a6eaffd6bb555ce571483bcd5b1912
NiharikaChoudhary <88475866+NiharikaChoudhary@users.noreply.github.com>
2024-01-06 17:20:47 +0530

    Add files via upload

--- src/Component/Caraousalservbottom.js
+++ src/Component/Caraousalservbottom.js
@@ -12,18 +12,19 @@ import '../caraousalservbottom.css'
 const CaraousalserveBottom = () => {
    
   return (
-
-
-     
-
-  
-      <div className="marquee-container">
-        <div className="marquee-content">
-        {/* Wash and fold ₹49    |    Wash and iron ₹69    |    dry clean ₹299    |    Open Laundry ₹250 per rotation */}
-          <p>Wash and fold ₹49  &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;Wash and iron ₹69 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; Dry clean ₹299  &nbsp;&nbsp;&nbsp;&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;Open Laundry ₹250 per rotation &nbsp;&nbsp;&nbsp;&nbsp;|</p>
+    <marquee class="marq"
+                 direction="left"
+                 loop="">
+            <div class="mq1">Wash and fold ₹49  &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;Wash and iron ₹69 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; Dry clean ₹299  &nbsp;&nbsp;&nbsp;&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;Open Laundry ₹250 per rotation &nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;&nbsp; Wash and fold ₹49  &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;Wash and iron ₹69 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; Dry clean ₹299  &nbsp;&nbsp;&nbsp;&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;Open Laundry ₹250 per rotation &nbsp;&nbsp;&nbsp;&nbsp;|</div>
+           
+        </marquee>
+      // <div className="marquee-container">
+      //   <div className="marquee-content">
+      //   {/* Wash and fold ₹49    |    Wash and iron ₹69    |    dry clean ₹299    |    Open Laundry ₹250 per rotation */}
+      //     <p>Wash and fold ₹49  &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;Wash and iron ₹69 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; Dry clean ₹299  &nbsp;&nbsp;&nbsp;&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;Open Laundry ₹250 per rotation &nbsp;&nbsp;&nbsp;&nbsp;|</p>
           
-        </div>
-      </div>
+      //   </div>
+      // </div>
     
 
   )
