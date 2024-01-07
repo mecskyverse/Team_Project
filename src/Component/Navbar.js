a0f6ad51cbdf034863ac8e78d073fa5117f4fe03
NiharikaChoudhary <88475866+NiharikaChoudhary@users.noreply.github.com>
2024-01-07 14:56:11 +0530

    Add files via upload

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import "../Project.css";
-import { FaBars } from 'react-icons/fa';
 import logo from "../Component/images/nav-logo.png";
+import { FaBars } from 'react-icons/fa';
 const Navbar = () => {
+  const token = localStorage.getItem("token")
   return (
-    // <div>
-<nav 
+    <div>
+<nav
           style={{ background: "#dce9d2" }}
-          className="navbar  position-static my-nav-bar navbar-expand-lg"
+          className="navbar my-nav-bar navbar-expand-lg"
         >
           <Link className="navbar-brand ml-3 w-50 my-links" to="/">
               <img
@@ -34,43 +35,53 @@ const Navbar = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ml-auto">
-            <li className="nav-item ml-3">
+            <li className="nav-item ml-4">
                 <Link className="nav-link font-weight-bold my-links" to="/">
                   Home
                 </Link>
               </li>
 
-              <li className="nav-item ml-3">
+              <li className="nav-item ml-4">
                 <Link className="nav-link font-weight-bold my-links" to="/services">
                   Services
                 </Link>
               </li>
-              <li className="nav-item ml-lg-1 ml-3">
+              <li className="nav-item ml-4">
                 <Link className="nav-link font-weight-bold my-links" to="/pickup">
                   Order Now
                 </Link>
               </li>
-              <li className="nav-item ml-lg-1 ml-3">
+              <li className="nav-item ml-4">
                 <Link className="nav-link font-weight-bold my-links" to="/#">
                   Contact Us
                 </Link>
               </li>
-              <li className="nav-item ml-3 mb-lg-1 mb-2">
+              {token ?
+               (<div className='d-flex'>
+              <li className="nav-item w-100 ml-4 mb-lg-1 mb-2">
               <Link to="/signup" className="btn py-1 px-4 my-nav-btn1 ">
                   Signup
                   </Link>
               </li>
-              <li className="nav-item ml-3 mr-3 ">
+              <li className="nav-item ml-4 w-100 mr-5 ">
               <Link to="/login" className="btn py-1 px-4 my-nav-btn2 ">
                   Login
                   </Link>
                 
               </li>
+              </div>): 
+               (<li className="nav-item ml-4 mr-5 ">
+               <Link to="/login" className="btn py-1 px-4 btn-primary ">
+                  Profile
+                  </Link>
+                
+             </li>)
+              }
             </ul>
           </div>
         </nav>
 
-    // </div>
+    </div>
   )
 }
 
