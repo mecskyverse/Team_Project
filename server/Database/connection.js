dd3258e54c75c96a9a2a30a2a7514a5c1896b8a2
Aakash <vermaaakash1000@gmail.com>
2024-01-13 20:33:40 +0530

    Database connection updated

--- server/Database/connection.js
+++ server/Database/connection.js
@@ -19,10 +19,10 @@
 
 const mysql = require('mysql2');
 const connection =  mysql.createConnection({
-  host: 'laughnlaundry.in',
+  host: '193.203.184.37',
+  database: 'u548717484_Laundry',
   user: 'u548717484_Laundry',
   password : "Laundry$13716",
-  database: 'u548717484_Laundry',
 });
 
 connection.connect((error)=>{
